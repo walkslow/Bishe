7b8aa6e4462407cbf886947deb2e3e5f0f66898f
walkslow <1617595782@qq.com>
2024-03-02 17:50:57 +0800

    为tools.py中的函数添加注释和文档字符串

--- pages/1_谱漂.py
+++ pages/1_谱漂.py
@@ -4,10 +4,12 @@ from io import BytesIO
 
 st.set_page_config("谱漂数据处理", layout="wide", initial_sidebar_state="collapsed")
 
+# 文件上传组件
 file_cols = st.columns(2)
 f1 = file_cols[0].file_uploader("上传室温下的能道数据文件", type=["txt", "csv", "xls", "xlsx"])
 f2 = file_cols[1].file_uploader("上传加热后的能道数据文件", type=["txt", "csv", "xls", "xlsx"])
 
+# 将室温下的能道数据df1和加热后的能道数据df2添加到session_state中，df1和df2均为包含channels和counts这2列的dataframe
 if "df1" not in st.session_state:
     st.session_state.df1 = None
 if "df2" not in st.session_state:
@@ -18,23 +20,27 @@ if f1 is not None:
 if f2 is not None:
     st.session_state.df2 = tls.get_df_from_file(f2)
 
+# 室温下谱图图形组件
 chart_cols = st.columns(2)
 with chart_cols[0]:
     chart1, container1 = st.columns([3, 1])
     with chart1:
         tls.show_chart(st.session_state.df1)
         channels_standard = [62, 108, 122, 227]
+    # 根据输入的channel查询室温下对应的counts值
     with container1:
         channel1 = st.number_input("channel:", key="channel1", min_value=1, max_value=256, value=None, step=1,
                                    format="%d")
         value = tls.get_counts(st.session_state.df1, channel1)
         counts1 = st.number_input("counts:", key="counts1", value=value, disabled=True)
 
+# 加热后谱图图形组件
 with chart_cols[1]:
     chart2, container2 = st.columns([3, 1])
     with chart2:
         tls.show_chart(st.session_state.df2)
         channels_real = [51, 89, 101, 187]
+    # 根据输入的channel查询加热后对应的counts值
     with container2:
         channel2 = st.number_input("channel:", key="channel2", min_value=1, max_value=256, value=None, step=1,
                                    format="%d")
@@ -43,6 +49,8 @@ with chart_cols[1]:
 
 st.write("谱漂图如下：")
 st.divider()
+
+# 谱漂图形组件
 chart3, container3 = st.columns([4, 1])
 with chart3:
     if st.session_state.df1 is not None and st.session_state.df2 is not None:
@@ -55,6 +63,8 @@ with chart3:
         st.download_button("谱漂数据下载", excel_data, file_name='after_data.xlsx')
     else:
         result = None
+
+# 根据输入的channel，查询counts_origin和counts_after
 with container3:
     channel3 = st.number_input("channel:", key="channel3", min_value=1, max_value=256, value=None, step=1, format="%d")
     value1 = tls.get_counts(result, channel3, col1='Channels', col2='Counts_origin')
--- tools.py
+++ tools.py
@@ -6,9 +6,16 @@ import altair as alt
 
 @st.cache_data(max_entries=2)
 def get_df_from_file(file):
-    # 使用 Pandas 读取 CSV 或 Excel 文件
-    if file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or file.type == "application/vnd.ms-excel":
+    """
+    接受st.file_uploader的返回值，将file文件转化为dataframe格式
+    :param file:
+    :return: df:dataframe格式，第一列为channels，第二列为counts
+    """
+    # 读取xlsx和xls文件，格式为包含channels和counts这2列数据，每列有256个数据
+    if (file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            or file.type == "application/vnd.ms-excel"):
         df = pd.read_excel(file, usecols=['channels', 'counts'])
+    # 读取txt文件，格式为1列数据，第1行为列名，从第2行到第257行为counts计数值
     else:
         df = pd.read_csv(file, skiprows=1, header=None, names=['counts'])
         df['channels'] = range(1, 257)
@@ -17,6 +24,11 @@ def get_df_from_file(file):
 
 
 def show_chart(df):
+    """
+    根据df显示折线图
+    :param df:2列，第1列为channels，第2列为counts
+    :return:
+    """
     line_chart = alt.Chart(df).mark_line().encode(
         x=alt.X("channels", type="ordinal"),
         y=alt.Y("counts", type="quantitative")
@@ -28,15 +40,23 @@ def show_chart(df):
 
 
 @st.cache_data()
-def get_counts(df, channel,col1='channels',col2='counts'):
-    if df is not None and channel is not None:
-        return df.loc[df[col1] == channel, col2].values[0]
+def get_counts(df, value, col1='channels', col2='counts'):
+    """
+    df包含col1和col2这2列，当col1的某一行的值为value时，返回col2的同一行的值
+    :param df:
+    :param value:
+    :param col1:
+    :param col2:
+    :return:
+    """
+    if df is not None and value is not None:
+        return df.loc[df[col1] == value, col2].values[0]
     return None
 
 
 @st.cache_data()
 def get_new_spectrum(df1, channels_standard, df2, channels_real, step_len):
-    '''
+    """
     根据室温和加热情况下的谱图及峰位，拟合出加热后谱漂情况下的谱图，返回谱漂下的计数值
     :param df1:室温下谱图
     :param channels_standard:室温下谱图的峰位
@@ -44,11 +64,11 @@ def get_new_spectrum(df1, channels_standard, df2, channels_real, step_len):
     :param channels_real:加热下谱图的峰位
     :param step_len:搜索步长
     :return:new_spectrum[]拟合后的计数值
-    '''
+    """
     # 多项式拟合
-    c2e_w = np.polyfit(channels_standard, channels_real, 1)
+    c2e_w = np.polyfit(channels_standard, channels_real, 1)  # 拟合系数
     c2e_s = np.polyfit(channels_real, channels_standard, 1)
-    c2e_w11 = np.poly1d(c2e_w)
+    c2e_w11 = np.poly1d(c2e_w)  # 拟合函数
     c2e_s11 = np.poly1d(c2e_s)
 
     new_spectrum_len = len(df2)
@@ -76,16 +96,19 @@ def get_new_spectrum(df1, channels_standard, df2, channels_real, step_len):
 
 
 def show_result_chart(old_spectrum, new_spectrum, channels_standard):
-    '''
+    """
     显示室温下和加热后拟合的谱图，观察谱漂现象
     :param old_spectrum: 室温下谱图信息，为dataframe，第一列为channels，第2列为counts
     :param new_spectrum: 加热拟合的谱图，为列表，保存了各个channel的counts信息
-    :return: result:包含3列的dataframe,分别为能道、原始计数、拟合计数
-    '''
+    :param channels_standard:室温下谱图的峰位值
+    :return: result:包含3列的dataframe,分别为能道Channels、原始计数Counts_origin、拟合计数Counts_after
+    """
+    # 将室温下谱图数据和加热拟合谱图数据合并为df_wide
     df_wide = pd.DataFrame(columns=['Channels', 'Counts_origin', 'Counts_after'])
     df_wide['Channels'] = range(1, 257)
     df_wide['Counts_origin'] = old_spectrum['counts']
     df_wide['Counts_after'] = new_spectrum
+    # 将宽格式的df_wide转化为长格式的df_long，便于altair作图
     df_long = df_wide.melt(id_vars=['Channels'], var_name='condition', value_name='Counts')
     line_chart = alt.Chart(df_long).mark_line().encode(
         x='Channels',
@@ -93,36 +116,14 @@ def show_result_chart(old_spectrum, new_spectrum, channels_standard):
         color='condition',
         tooltip=['Channels', 'Counts']
     ).interactive()
-    vlines=alt.Chart(pd.DataFrame({'Channels_standard': channels_standard})).mark_rule(color='red', strokeDash=[5,5]).encode(
+    # 显示峰位值的几条垂直虚线
+    v_lines = alt.Chart(
+        pd.DataFrame({'Channels_standard': channels_standard})
+    ).mark_rule(
+        color='red', strokeDash=[5, 5]
+    ).encode(
         x='Channels_standard'
     ).interactive()
-    st.altair_chart(line_chart+vlines,use_container_width=True)
+    # 将折线图与峰位值虚线叠加显示
+    st.altair_chart(line_chart + v_lines, use_container_width=True)
     return df_wide
-
-
-def data_input(uploaded_file):
-    """
-    :param uploaded_file
-    :return: df
-    """
-    # 如果用户上传了文件，读取文件内容并转换为 DataFrame
-    if uploaded_file is not None:
-        try:
-            # 使用 Pandas 读取 TXT 或 Excel 文件
-            if uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-                df = pd.read_excel(uploaded_file, usecols=['channels', 'counts'])
-            else:
-                df = pd.read_csv(uploaded_file, skiprows=1, header=None, names=['counts'])
-                df['channels'] = range(1, 257)
-                df = df[['channels', 'counts']]
-
-            # 显示 DataFrame
-            st.dataframe(df, use_container_width=True, hide_index=True)
-            line_chart = alt.Chart(df).mark_line().encode(
-                x="channels",
-                y="counts"
-            ).interactive()
-            st.altair_chart(line_chart, use_container_width=True)
-            return df
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
